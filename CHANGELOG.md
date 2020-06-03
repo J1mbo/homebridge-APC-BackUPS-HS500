6ec0c97caa36ece631d58d0b6e251d2574e6bdb1
J1mbo <pearce_jj@hotmail.com>
2020-06-03 21:38:40 +0100

    Initial commit - 03-Jun-20, James Pearce

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,34 +1,8 @@
-# [0.1.0]
 
-- Publish battery service in Home friendly way
+# [1.0.0]
 
-# [0.0.8]
-
-- Fix client promise handling
-
-# [0.0.7]
-
-- ESLint and Prettier
-
-# [0.0.5/0.0.6]
-
-- Cache the JSON result
-- Refactor state
-- Secure NPM profile and add keywords
-
-# [0.0.4]
-
-- Tweak name config
-
-# [0.0.3]
-
-- Learned how to push changes to homekit using updateValue. So now it alerts properly!
-- We're due a refactor now. But it works!
-
-# [0.0.2]
-
-- Added Battery statistics (on Sensor; in future we may want to reveal as BatteryService)
+- Initial release based on external XML watcher
 
 # [0.0.1]
 
-- Contact State reported on enquiry
+- Fork from cr3ative/homebridge-apcaccess
--- index.js
+++ index.js
@@ -1,127 +1,178 @@
-let Service;
-let Characteristic;
-const ApcAccess = require('apcaccess');
+// Plugin for the APC UPS monitoring
+// Copyright (c) James Pearce, 2020
+// Last updated May 2020 Revision 1
 
-class APCAccess {
-  constructor(log, config) {
-    this.log = log;
-    this.latestJSON = false;
+// globals and imports
+var exec = require('child_process').exec;
+const jsdom = require("jsdom");
 
-    this.client = new ApcAccess();
-    this.client
-      .connect(config.host || 'localhost', config.port || '3551')
-      .then(() => {
-        this.log('Connected!');
-        // set up watcher
-        setInterval(this.getLatestJSON.bind(this), (config.interval || 1) * 1000);
-      })
-      .catch((err) => {
-        this.log("Couldn't connect to service:", err);
-      });
+// HomeKit API registration
+module.exports = (api) => {
+  api.registerAccessory('APCBackUpsHS500', APCBackUpsHS500);
+}
 
-    this.state = {
-      contact: 0,
-      lowBattery: 0,
-    };
 
-    // The following can't be defined on boot, so define them optionally in config
-    this.contactSensor = new Service.ContactSensor(config.name || 'APCAccess UPS');
-    this.informationService = new Service.AccessoryInformation();
-    this.informationService
-      .setCharacteristic(
-        Characteristic.Manufacturer,
-        config.manufacturer || 'American Power Conversion',
-      )
-      .setCharacteristic(Characteristic.Model, config.model || 'APCAccess UPS')
-      .setCharacteristic(Characteristic.SerialNumber, config.serial || '0118-999-88199-9119-725-3');
-    // End of vanity values ;)
+class APCBackUpsHS500 {
 
-    this.batteryService = new Service.BatteryService();
+  constructor(log, config, api) {
+      this.log = log;
+      this.config = config;
+      this.api = api;
 
-    this.contactSensor
-      .getCharacteristic(Characteristic.ContactSensorState)
-      .on('get', this.getContactState.bind(this));
-    this.batteryService
-      .getCharacteristic(Characteristic.BatteryLevel)
-      .on('get', this.getBatteryLevel.bind(this));
-    this.batteryService
-      .getCharacteristic(Characteristic.ChargingState)
-      .on('get', this.getChargingState.bind(this));
-    this.batteryService
-      .getCharacteristic(Characteristic.StatusLowBattery)
-      .on('get', this.getStatusLowBattery.bind(this));
-  }
+      this.Service = this.api.hap.Service;
+      this.Characteristic = this.api.hap.Characteristic;
 
+      this.name = config.name || 'APC UPS';
+      this.model = config.model || 'APC UPS Type';
+      this.upsIpAddress = config.upsIpAddress;
+      this.statusCommand = config.statusCommand;
+      this.serialNumber = config.serialNumber;
+      this.pollTimer = config.pollTimer || 30; //default poll interval = 30 seconds
+      this.lowBattery = config.lowBattery || 20; // default warn at 20% remaining
+
+      this.state = {
+        contactSensorState: 0,
+        batteryLevel: 0,
+        chargingState: 0,
+        statusLowBattery: 0,
+      };
+
+      // The following can't be defined on boot, so define them optionally in config
+      this.contactSensor = new this.Service.ContactSensor(this.name);
+
+      // create an information service...
+      this.informationService = new this.Service.AccessoryInformation()
+        .setCharacteristic(this.Characteristic.Manufacturer, "APC")
+        .setCharacteristic(this.Characteristic.Model, this.model)
+        .setCharacteristic(this.Characteristic.SerialNumber, this.serialNumber);
+
+      this.batteryService = new this.Service.BatteryService();
+
+      this.contactSensor
+        .getCharacteristic(this.Characteristic.ContactSensorState)
+        .on('get', this.getContactState.bind(this));
+      this.batteryService
+        .getCharacteristic(this.Characteristic.BatteryLevel)
+        .on('get', this.getBatteryLevel.bind(this));
+      this.batteryService
+        .getCharacteristic(this.Characteristic.ChargingState)
+        .on('get', this.getChargingState.bind(this));
+      this.batteryService
+        .getCharacteristic(this.Characteristic.StatusLowBattery)
+        .on('get', this.getStatusLowBattery.bind(this));
+  } // constructor
+
+  // mandatory getServices function tells HomeBridge how to use this object
   getServices() {
-    // Required by Homebridge; expose services this accessory claims to have
-    return [this.informationService, this.contactSensor, this.batteryService];
-  }
-
-  getLatestJSON() {
-    this.client.getStatusJson().then((result) => {
-      this.latestJSON = result;
-      this.doPolledChecks();
-    });
-  }
+    return [
+      this.informationService,
+      this.contactSensor,
+      this.batteryService,
+    ];
+  } // getServices()/
 
   getBatteryLevel(callback) {
-    // BCHARGE
-    const percentage = parseInt(this.latestJSON.BCHARGE, 10);
-    this.log('Battery Level: ', percentage);
-    callback(null, percentage);
+    var accessory = this;
+    accessory.log.debug('Battery Level: ', accessory.batteryLevel);
+    callback(null, accessory.batteryLevel);
   }
 
   getChargingState(callback) {
-    // STATFLAG
-    const percentage = parseInt(this.latestJSON.BCHARGE, 10);
-    const value = this.latestJSON.STATFLAG & 0x80
-      ? 'NOT_CHARGEABLE'
-      : this.latestJSON.STATFLAG & 0x10 || percentage === 100
-        ? 'NOT_CHARGING'
-        : 'CHARGING';
-    this.log('Charging state: ', value);
-    callback(null, Characteristic.ChargingState[value]);
+    var accessory = this;
+    accessory.log.debug('Charging State: ', accessory.chargingState);
+    callback(null, accessory.chargingState);
   }
 
   getStatusLowBattery(callback) {
-    // STATFLAG
-    const value = this.latestJSON.STATFLAG & 0x40 ? 'BATTERY_LEVEL_LOW' : 'BATTERY_LEVEL_NORMAL';
-    this.log('Low Battery? ', value);
-    callback(null, Characteristic.StatusLowBattery[value]);
+    var accessory = this;
+    accessory.log.debug('Status Low Battery: ', accessory.statusLowBattery);
+    callback(null, accessory.statusLowBattery);
   }
 
   getContactState(callback) {
-    const value = [this.latestJSON.STATFLAG & 0x08 ? 'CONTACT_DETECTED' : 'CONTACT_NOT_DETECTED'];
-    callback(null, Characteristic.ContactSensorState[value]);
-  }
+    var accessory = this;
+    var Characteristic = this.Characteristic;
+    accessory.log.debug('Contact State (=On Mains flag): ', accessory.contactState);
 
-  doPolledChecks() {
-    const contactValue = [
-      this.latestJSON.STATFLAG & 0x08 ? 'CONTACT_DETECTED' : 'CONTACT_NOT_DETECTED',
-    ];
-    const contactBool = Characteristic.ContactSensorState[contactValue];
-    const lowBattValue = this.latestJSON.STATFLAG & 0x40 ? 'BATTERY_LEVEL_LOW' : 'BATTERY_LEVEL_NORMAL';
-    const lowBattBool = Characteristic.StatusLowBattery[lowBattValue];
-    // push
-    if (this.state.contact !== contactBool) {
-      console.log('Pushing contact state change; ', contactBool, this.state.contact);
-      this.contactSensor
-        .getCharacteristic(Characteristic.ContactSensorState)
-        .updateValue(contactBool);
-      this.state.contact = contactBool;
-    }
-    if (this.state.lowBattery !== lowBattBool) {
-      console.log('Pushing low battery state change; ', lowBattBool, this.state.lowBattery);
-      this.contactSensor
-        .getCharacteristic(Characteristic.StatusLowBattery)
-        .updateValue(lowBattBool);
-      this.state.lowBattery = lowBattBool;
-    }
-  }
-}
+    var command = accessory.statusCommand + " " + accessory.upsIpAddress;
+    accessory.log.debug('Running ' + command);
 
-module.exports = (homebridge) => {
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory('homebridge-apcaccess', 'APCAccess', APCAccess);
-};
+    exec(command, function (err, stdout, stderr) {
+      if (err) {
+        accessory.log('Error: ' + err + ' ' + stderr);
+        if (callback) callback(err || new Error('Error getting state of ' + accessory.name));
+      } else {
+        // got something back
+        var xmlDoc = new jsdom.JSDOM(stdout);
+        if (xmlDoc.window.document.querySelector("parseerror")) {
+          // text wasn't XML
+          var array = stdout.toString().split("\n");
+          accessory.log('XML error (got: ' + !array[0] + ')');
+          if (callback) callback(err || new Error('Error getting state of ' + accessory.name));
+        } else {
+          // valid XML returned, hopefully with all the values we need
+          accessory.log.debug( xmlDoc.window.document.querySelector("UPSSTATUS").textContent );
+          if (xmlDoc.window.document.querySelector("UPSSTATUS").textContent != 'On Line') {
+            accessory.state.contactSensorState = 1 // contact is ON, i.e. ALERT STATE (on battery)
+          } else {
+            accessory.state.contactSensorState = 0 // contact is OFF, i.e. normal conditions (on mains)
+          }
+
+          accessory.log.debug( xmlDoc.window.document.querySelector("BATTERYLEVEL").textContent );
+          accessory.state.batteryLevel = xmlDoc.window.document.querySelector("BATTERYLEVEL").textContent;
+          if (accessory.state.batteryLevel < accessory.lowBattery) {
+            accessory.state.statusLowBattery = 1 // BATTERY_LEVEL_LOW
+          } else {
+            accessory.state.statusLowBattery = 0 // BATTERY_LEVEL_NORMAL
+          }
+
+          accessory.state.chargingState = 0; // Assume NOT_CHARGING otherwise
+          accessory.log.debug( xmlDoc.window.document.querySelector("BATTERYSTATUS").textContent );
+          if (xmlDoc.window.document.querySelector("BATTERYSTATUS").textContent == 'Charging') {
+            accessory.state.chargingState = 1; // CHARGING
+          }
+          if (xmlDoc.window.document.querySelector("BATTERYSTATUS").textContent == 'Charged') {
+            accessory.state.chargingState = 1; // CHARGING
+          }
+
+          accessory.contactSensor.updateCharacteristic(Characteristic.ContactSensorState, accessory.state.contactSensorState);
+          accessory.batteryService.updateCharacteristic(Characteristic.BatteryLevel, accessory.state.batteryLevel);
+          accessory.batteryService.updateCharacteristic(Characteristic.ChargingState, accessory.state.chargingState);
+          accessory.batteryService.updateCharacteristic(Characteristic.StatusLowBattery, accessory.state.statusLowBattery);
+
+          if (callback) {
+            callback(null, accessory.state.contactSensorState);
+          }
+        }
+      } // if (err) / else
+    }); // exec
+    accessory.pollState(); // (re)start polling timer
+  } // getState
+
+  /**
+    * Polling function
+  */
+  pollState = function() {
+    var accessory = this;
+    var Characteristic = this.Characteristic;
+
+    // Clear any existing timer
+    if (accessory.stateTimer) {
+      clearTimeout(accessory.stateTimer);
+      accessory.stateTimer = null;
+    }
+
+    // define the new poll function
+    accessory.stateTimer = setTimeout(
+      function() {
+        accessory.getContactState(function(err, CurrentDeviceState) {
+          if (err) {
+            accessory.log(err);
+            return;
+          }
+        })
+      }, accessory.pollTimer * 1000
+    );
+  } // pollState
+
+} // class APCaccess
